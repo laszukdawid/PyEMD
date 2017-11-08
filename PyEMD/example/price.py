dcf96b4b31550aa02b455f6e353b9f219bf3b1ae
liuchangyu1988 <liuchangyu1988@gmail.com>
2017-11-08 00:25:01 -0500

    Added two util functionalities: 1) reformat the data file 2) resample the data file to extract the minute close, open, high and low

--- PyEMD/example/price.py
+++ PyEMD/example/price.py
@@ -2,24 +2,32 @@
 # Last update: 10/24/2017
 from __future__ import division, print_function
 import sys
-sys.path.append('/Users/cliu270/Projects/PyEMD')
+sys.path.append('C:/Users/Chang/PycharmProjects/PyEMD')
 
 import numpy  as np
 import pylab as plt
 from PyEMD import EMD
+from Util import parser
 import pandas as pd
 import os
 
 # Read price data from file
-price_table = pd.read_csv("C:/Users/Chang/Documents/Data/AAPL.csv")
-time_column = price_table.Time
-close_column = price_table.Close
+#price_table = pd.read_csv("C:/Users/Chang/Documents/Data/AAPL.csv")
+open_table, close_table, high_table, low_table = parser.parse_file("D:/Documentations/For Ownership/Data/EURGBP_2017_01_02_X_Q_Processed.txt")
+
+close_table['raw_index'] = range(0, len(close_table))
+#close_table.set_index('raw_index')
+#print (close_table)
+time_column = close_table.raw_index
+close_column = close_table.Bid
+
+print (close_table)
 #time_column = time_column.values
 #close_column = close_column.values
 time_column = time_column.as_matrix(columns=None)
 close_column = close_column.as_matrix(columns=None)
-time_column = time_column[:200]
-close_column = close_column[:200]
+#time_column = time_column[:200]
+#close_column = close_column[:200]
 #print (time_column)
 #print (close_column)
 
