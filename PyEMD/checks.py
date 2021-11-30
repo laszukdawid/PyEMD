c323f6b5dd1cc5b9c03420fb2e811c66d1146e19
Arnab Paul Choudhury <arnabpc.oshin94@gmail.com>
2021-12-01 02:45:55 +0530

    Changes in checks.py and test_checks.py \> \> Added docstring to the files in accordance with pep257 and checked with pep257 tool. Added test cases, fixed IMF input, removed normalize function in checks.py. Other minor changes.

--- PyEMD/checks.py
+++ PyEMD/checks.py
@@ -1,59 +1,63 @@
-from PyEMD import EMD, EEMD, CEEMDAN
+"""Calculate the statistical Significance of IMFs."""
 import numpy as np
 from scipy import stats
 import math
-from scipy.signal import argrelextrema
-
-#helper function: normalize data
-def normalize(data):
-    return (data - data.mean())/(data.max() - data.min())
+from scipy.signal import find_peaks
 
 #helper function: Find mean period of an IMF
 def mean_period(data):
-    return len(data)/len(argrelextrema(data, np.greater)[0])
+    """Return mean-period of signal."""
+    peaks = len(find_peaks(data, height=0)[0])
+    return len(data)/peaks if peaks > 0 else len(data)
 
 #helper function: find energy of signal/IMF
 def energy(data):
+    """Return energy of signal."""
     return sum(pow(data, 2))
 
 #helper function: find IMF significance in 'a priori' test
 def significance_apriori(energy_density, T, N, alpha):
+    """Check a priori significance and Return True if significant else False."""
     k = abs(stats.norm.ppf((1-alpha)/2))
-    upper_limit = -math.log(T) + ( k * (math.sqrt(2/N) * math.exp(math.log(T)/2)) )
-    lower_limit = -math.log(T) - ( k * (math.sqrt(2/N) * math.exp(math.log(T)/2)) )
+    upper_limit = -T + ( k * (math.sqrt(2/N) * math.exp(T/2)) )
+    lower_limit = -T - ( k * (math.sqrt(2/N) * math.exp(T/2)) )
 
     return not (lower_limit <= energy_density <= upper_limit)
 
 
 #helper function: find significance in 'a posteriori' test
 def significance_aposteriori(scaled_energy_density, T, N, alpha):
+    """Check a posteriori significance and Return True if significant else False."""
     k = abs(stats.norm.ppf((1-alpha)/2))
-    lower_limit = -math.log(T) + ( k * (math.sqrt(2/N) * math.exp(math.log(T)/2)) )
+    upper_limit = -T + ( k * (math.sqrt(2/N) * math.exp(T/2)) )
+    return not (scaled_energy_density <= upper_limit)
 
-    return not (scaled_energy_density <= lower_limit)
 
-
-def whitenoise_check(data: np.ndarray, method: str='EMD',  test: str='aposteriori', rescaling_imf: int=1, alpha: float= 0.95, **kwargs):
-    """Returns a dictionary with IMF number(index starts at 1) as key; 0 value means IMF has noise in it and is not statistically significant; 1 value means IMF has some information and is statistically significant
+def whitenoise_check(IMFs: np.ndarray,  test: str='aposteriori', rescaling_imf: int=1, alpha: float= 0.95):
+    """Whitenoise statistical significance test.
 
     References
     ----------
-    -- Zhaohua Wu, and Norden E. Huang. “A Study of the Characteristics of White Noise Using the Empirical Mode Decomposition Method.” Proceedings: Mathematical, Physical and Engineering Sciences, vol. 460, no. 2046, The Royal Society, 2004, pp. 1597–611, http://www.jstor.org/stable/4143111.
+    -- Zhaohua Wu, and Norden E. Huang. “A Study of the Characteristics of White Noise Using the
+       Empirical Mode Decomposition Method.” Proceedings: Mathematical, Physical and Engineering
+       Sciences, vol. 460, no. 2046, The Royal Society, 2004, pp. 1597–611, http://www.jstor.org/stable/4143111.
 
     Parameters
     ----------
-    data: np.ndarray
-        (Required) The original signal/time-series in the form of a numpy.ndarray, Note: Sending decomposed IMFs as an input will not give proper results
-    method: str
-        (optional) Default value in 'EMD'. it can be used to select other decomposition methods like 'EMD', 'CEEMDAN'
+    IMFs: np.ndarray
+        (Required) numpy array containing IMFs computed from a normalized signal
     test: str
-        (Optional) Default value is 'aposteriori'. It can be used to select other test types like 'apriori'
+        (Optional) It can be used to select other test types like 'apriori'. (default 'aposteriori')
     rescaling_imf: int
-        (Optional) Default IMF number(index starts at 1) used for scaling in 'a posteriori test' is 1. rescaling_imf = ith IMF of the signal. Basically, if we are aware that a certain IMF is purely composed of noise we can use that rescale all the other IMFs.
+        (Optional) ith IMF of the signal used in rescaling for 'a posteriori' test. (default 1)
     alpha:
-        (Optional) Default is 0.95 or 95%. The percentiles at which the test is to be performed; 0 < alpha < 1;
-    **kwargs:
-        The keyword arguments for configuring the decomposition method of choice.
+        (Optional) The percentiles at which the test is to be performed; 0 < alpha < 1; (default 0.95)
+
+    Returns
+    -------
+    Dictionary with keys as ith IMF,
+    Values: 0, indicates IMF is not significant and has noise
+            1, indicates IMF is significant and has some information
 
     Examples
     --------
@@ -67,46 +71,40 @@ def whitenoise_check(data: np.ndarray, method: str='EMD',  test: str='aposterior
     >>> type(significant_imfs)
     <class 'dict'>
     """
-
-    assert alpha > 0 and alpha < 1, "alpha value should be in between (0,1)"
-    assert method == 'EMD' or method == 'EEMD' or method == 'CEEMDAN', 'Invalid decomposition method'
+    assert 0 < alpha < 1, "alpha value should be in between (0,1)"
     assert test == 'apriori' or test == 'aposteriori', 'Invalid test type'
-    assert isinstance(data, np.ndarray), 'Invalid Data type, Pass a numpy.ndarray'
-
-    N = len(data)
-    if method == 'EMD':
-        emd = EMD(**kwargs)
-        IMFs = emd.emd(normalize(data))
-    elif method == 'EEMD':
-        eemd = EEMD(**kwargs)
-        IMFs = eemd.eemd(normalize(data))
-    elif method == 'CEEMDAN':
-        ceemdan = CEEMDAN(**kwargs)
-        IMFs = ceemdan.ceemdan(normalize(data))
-
+    assert isinstance(IMFs, np.ndarray), 'Invalid Data type, Pass a numpy.ndarray containing IMFs'
     assert rescaling_imf > 0 and rescaling_imf <= len(IMFs), 'Invalid rescaling IMF'
 
-    output = {}
+    N = len(IMFs[0])
+    output ={}
+    if N == 0:
+        return {}
+    if np.isnan(np.sum(IMFs)) == True:
+        #Return NaN if input has NaN
+        return None
+
     if test == 'apriori':
         for idx,imf in enumerate(IMFs):
-            T = mean_period(imf)
-            energy_density = energy(imf)/N
-            sig_priori = significance_apriori(math.log(energy_density), T, N, alpha)
+            T = math.log(mean_period(imf))
+            energy_density = math.log(energy(imf)/N)
+            sig_priori = significance_apriori(energy_density, T, N, alpha)
 
             output[idx+1] = int(sig_priori)
 
     elif test == 'aposteriori':
-        scaling_imf_mean_period = mean_period(IMFs[rescaling_imf-1])
-        scaling_imf_energy_density = energy(IMFs[rescaling_imf-1])/N
+        scaling_imf_mean_period = math.log(mean_period(IMFs[rescaling_imf-1]))
+        scaling_imf_energy_density = math.log(energy(IMFs[rescaling_imf-1])/N)
 
         k = abs(stats.norm.ppf((1-alpha)/2))
-        upper_limit = -math.log(scaling_imf_mean_period) + ( k * math.sqrt(2/N)*math.exp(math.log(scaling_imf_mean_period)/2) )
+        up_limit = -scaling_imf_mean_period+(k*math.sqrt(2/N)*math.exp(scaling_imf_mean_period)/2)
 
-        scaling_factor = upper_limit - math.log(scaling_imf_energy_density)
+        scaling_factor = up_limit-scaling_imf_energy_density
+
         for idx,imf in enumerate(IMFs):
-            T = mean_period(imf)
-            energy_density = energy(imf)/N
-            scaled_energy_density = math.log(energy_density) + scaling_factor
+            T = math.log(mean_period(imf))
+            energy_density = math.log(energy(imf)/N)
+            scaled_energy_density = energy_density + scaling_factor
             sig_aposteriori = significance_aposteriori(scaled_energy_density, T, N, alpha)
 
             output[idx+1] = int(sig_aposteriori)
--- PyEMD/tests/test_checks.py
+++ PyEMD/tests/test_checks.py
@@ -1,47 +1,85 @@
+"""Tests for checks.py."""
 import numpy as np
 import unittest
-from PyEMD.significancetest import normalize
-from PyEMD.significancetest import mean_period
-from PyEMD.significancetest import energy
-from PyEMD.significancetest import significance_apriori
-from PyEMD.significancetest import significance_aposteriori
-from PyEMD.significancetest import whitenoise_check
-from PyEMD.significancetest import normalize
+from PyEMD.checks import mean_period
+from PyEMD.checks import energy
+from PyEMD.checks import significance_apriori
+from PyEMD.checks import significance_aposteriori
+from PyEMD.checks import whitenoise_check
 
 class TestCase(unittest.TestCase):
-    def test_normalize(self):
-        T = np.linspace(0, 1, 100)
-        norm = normalize(T)
-        self.assertEqual(len(T), len(norm), "Lengths must be equal")
-
+    '''Test cases.'''
     def test_mean_period(self):
-        T = np.linspace(0, 2, 200)
-        S = np.sin(2*2*np.pi*T)
+        '''Test to check if mean period output is correct.'''
+        T = np.linspace(0, 2, 100)
+        S = np.sin(2*np.pi*T)
         res = mean_period(S)
         self.assertEqual(type(res), float, "Default data type is float")
+        self.assertTrue(res > 0, "mean-period cannot be zero")
+
+    def test_mean_period_zero_peaks(self):
+        '''Tect to check if mean period function can handle zero peaks.'''
+        T = np.linspace(0, 2, 100)
+        res = mean_period(T)
+        self.assertEqual(res, len(T), "mean-period is same as signal length in case of monotonic curve")
 
     def test_energy(self):
+        '''Test to check if energy of signal is being computed properly.'''
         T = np.linspace(0, 2, 200)
         S = np.sin(2*2*np.pi*T)
         res = energy(S)
         self.assertEqual(type(res), np.float64, "Default data type is float")
 
     def test_significance_apriori(self):
+        '''a priori significance test.'''
         T = np.linspace(0, 2, 200)
         S = np.sin(2*2*np.pi*T)
-        res = significance_apriori(S, 2, len(S), 0.095)
+        energy_density = energy(S)/len(S)
+        res = significance_apriori(energy_density, 2, len(S), 0.9)
         self.assertEqual(type(res), bool, "Default data type is bool")
 
     def test_significance_aposteriori(self):
+        '''a posteriori significance test.'''
         T = np.linspace(0, 2, 200)
         S = np.sin(2*2*np.pi*T)
-        res = significance_aposteriori(S, 2, len(S), 0.095)
+        energy_density = energy(S)/len(S)
+        res = significance_aposteriori(energy_density, 2, len(S), 0.9)
         self.assertEqual(type(res), bool, "Default data type is bool")
 
-    def test_whitenoise_check(self):
-        T = np.linspace(0, 2, 200)
-        S = np.sin(2*2*np.pi*T)
-        res = whitenoisecheck(S)
+    def test_whitenoise_check_apriori(self):
+        '''a priori whitenoise_check.'''
+        T = [np.linspace(0, i, 200) for i in range(5,0,-1)]
+        S = np.array([list(np.sin(2*2*np.pi*i)) for i in T])
+        res = whitenoise_check(S, test='apriori')
         self.assertEqual(type(res), dict or None, "Default data type is dict")
+    
+    def test_whitenoise_check_apriori_alpha(self):
+        '''a priori whitenoise_check with custom alpha'''
+        T = [np.linspace(0, i, 200) for i in range(5,0,-1)]
+        S = np.array([list(np.sin(2*2*np.pi*i)) for i in T])
+        res = whitenoise_check(S, test='apriori', alpha = 0.99)
+        self.assertEqual(type(res), dict or None, "Default data type is dict")
+
+    def test_whitenoise_check_alpha(self):
+        '''a posteriori whitenoise check with custom alpha value.'''
+        T = [np.linspace(0, i, 200) for i in range(5,0,-1)]
+        S = np.array([list(np.sin(2*2*np.pi*i)) for i in T])
+        res = whitenoise_check(S, alpha=0.9)
+        self.assertEqual(type(res), dict or None, "Default data type is dict")
+
+    def test_whitenoise_check_rescaling_imf(self):
+        '''a posteriori whitenoise check with custom rescaling imf.'''
+        T = [np.linspace(0, i, 200) for i in range(5,0,-1)]
+        S = np.array([list(np.sin(2*2*np.pi*i)) for i in T])
+        res = whitenoise_check(S, rescaling_imf=2)
+        self.assertEqual(type(res), dict or None, "Default data type is dict")
+
+    def test_whitenoise_check_nan_values(self):
+        '''whitenoise check with nan in IMF.'''
+        S = np.array([np.full(100, np.NaN) for i in range(5,0,-1)])
+        res = whitenoise_check(S)
+        self.assertEqual(res, None, "Input NaN return None")
+
+
 if __name__ == "__main__":
     unittest.main()
