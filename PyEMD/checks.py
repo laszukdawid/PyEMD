e37640ecf1784338d094cd9047c8db9772fd349d
oshin94 <34067069+oshin94@users.noreply.github.com>
2021-12-02 07:10:49 +0530

    #104 - Added IMF significance test "whitenoise check" (#105)

---
+++ PyEMD/checks.py
@@ -0,0 +1,130 @@
+"""Calculate the statistical Significance of IMFs."""
+import logging
+import math
+
+import numpy as np
+from scipy import stats
+from scipy.signal import find_peaks
+
+
+# helper function: Find mean period of an IMF
+def mean_period(data):
+    """Return mean-period of signal."""
+    peaks = len(find_peaks(data, height=0)[0])
+    return len(data) / peaks if peaks > 0 else len(data)
+
+
+# helper function: find energy of signal/IMF
+def energy(data):
+    """Return energy of signal."""
+    return sum(pow(data, 2))
+
+
+# helper function: find IMF significance in 'a priori' test
+def significance_apriori(energy_density, T, N, alpha):
+    """Check a priori significance and Return True if significant else False."""
+    k = abs(stats.norm.ppf((1 - alpha) / 2))
+    upper_limit = -T + (k * (math.sqrt(2 / N) * math.exp(T / 2)))
+    lower_limit = -T - (k * (math.sqrt(2 / N) * math.exp(T / 2)))
+
+    return not (lower_limit <= energy_density <= upper_limit)
+
+
+# helper function: find significance in 'a posteriori' test
+def significance_aposteriori(scaled_energy_density, T, N, alpha):
+    """Check a posteriori significance and Return True if significant else False."""
+    k = abs(stats.norm.ppf((1 - alpha) / 2))
+    upper_limit = -T + (k * (math.sqrt(2 / N) * math.exp(T / 2)))
+    return not (scaled_energy_density <= upper_limit)
+
+
+def whitenoise_check(IMFs: np.ndarray, test: str = "aposteriori", rescaling_imf: int = 1, alpha: float = 0.95):
+    """Whitenoise statistical significance test.
+
+    References
+    ----------
+    -- Zhaohua Wu, and Norden E. Huang. “A Study of the Characteristics of White Noise Using the
+       Empirical Mode Decomposition Method.” Proceedings: Mathematical, Physical and Engineering
+       Sciences, vol. 460, no. 2046, The Royal Society, 2004, pp. 1597–611, http://www.jstor.org/stable/4143111.
+
+    Parameters
+    ----------
+    IMFs: np.ndarray
+        (Required) numpy array containing IMFs computed from a normalized signal
+    test: str
+        (Optional) It can be used to select other test types like 'apriori'. (default 'aposteriori')
+    rescaling_imf: int
+        (Optional) ith IMF of the signal used in rescaling for 'a posteriori' test. (default 1)
+    alpha:
+        (Optional) The percentiles at which the test is to be performed; 0 < alpha < 1; (default 0.95)
+
+    Returns
+    -------
+    Dictionary with keys as ith IMF,
+    Values: 0, indicates IMF is not significant and has noise
+            1, indicates IMF is significant and has some information
+            None, if input IMFs have NaN/empty, check is skipped
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from PyEMD.significancetest import whitenoisecheck
+    >>> T = np.linspace(0, 1, 100)
+    >>> S = np.sin(2*2*np.pi*T)
+    >>> significant_imfs = whitenoisecheck(S, test='apriori')
+    >>> significant_imfs
+    {1: 0, 2: 1}
+    >>> type(significant_imfs)
+    <class 'dict'>
+    """
+    assert isinstance(IMFs, np.ndarray), "Invalid Data type, Pass a numpy.ndarray containing IMFs"
+    # check if IMFs are empty or not
+    if len(IMFs) == 0:
+        logging.getLogger("PyEMD").warning("Detected empty input. Skipping check.")
+        return None
+    if len(IMFs[0]) == 0:
+        logging.getLogger("PyEMD").warning("Detected empty input. Skipping check.")
+        return None
+
+    assert isinstance(alpha, float), "Invalid Data type for alpha, pass a float value between (0,1)"
+    assert 0 < alpha < 1, "alpha value should be in between (0,1)"
+    assert test == "apriori" or test == "aposteriori", "Invalid test type"
+    assert isinstance(rescaling_imf, int), "Invalid data type for rescaling_imf, pass a int value"
+    assert 0 < rescaling_imf <= len(IMFs), "Invalid rescaling IMF"
+
+    if np.isnan(np.sum(IMFs)):
+        # Return NaN if input has NaN
+        logging.getLogger("PyEMD").warning("Detected NaN values during whitenoise check. Skipping check.")
+        return None
+
+    N = len(IMFs[0])
+    output = {}
+    if test == "apriori":
+        for idx, imf in enumerate(IMFs):
+            log_T = math.log(mean_period(imf))
+            energy_density = math.log(energy(imf) / N)
+            sig_priori = significance_apriori(energy_density, log_T, N, alpha)
+
+            output[idx + 1] = int(sig_priori)
+
+    elif test == "aposteriori":
+        scaling_imf_mean_period = math.log(mean_period(IMFs[rescaling_imf - 1]))
+        scaling_imf_energy_density = math.log(energy(IMFs[rescaling_imf - 1]) / N)
+
+        k = abs(stats.norm.ppf((1 - alpha) / 2))
+        up_limit = -scaling_imf_mean_period + (k * math.sqrt(2 / N) * math.exp(scaling_imf_mean_period) / 2)
+
+        scaling_factor = up_limit - scaling_imf_energy_density
+
+        for idx, imf in enumerate(IMFs):
+            log_T = math.log(mean_period(imf))
+            energy_density = math.log(energy(imf) / N)
+            scaled_energy_density = energy_density + scaling_factor
+            sig_aposteriori = significance_aposteriori(scaled_energy_density, log_T, N, alpha)
+
+            output[idx + 1] = int(sig_aposteriori)
+
+    else:
+        raise AssertionError("Only 'apriori' and 'aposteriori' are allowed")
+
+    return output
---
+++ PyEMD/tests/test_checks.py
@@ -0,0 +1,124 @@
+"""Tests for checks.py."""
+import unittest
+
+import numpy as np
+
+from PyEMD.checks import energy, mean_period, significance_aposteriori, significance_apriori, whitenoise_check
+
+
+class TestCase(unittest.TestCase):
+    """Test cases."""
+
+    def test_mean_period(self):
+        """Test to check if mean period output is correct."""
+        T = np.linspace(0, 2, 100)
+        S = np.sin(2 * np.pi * T)
+        res = mean_period(S)
+        self.assertEqual(type(res), float, "Default data type is float")
+        self.assertTrue(res > 0, "mean-period cannot be zero")
+
+    def test_mean_period_zero_peaks(self):
+        """Tect to check if mean period function can handle zero peaks."""
+        T = np.linspace(0, 2, 100)
+        res = mean_period(T)
+        self.assertEqual(res, len(T), "mean-period is same as signal length in case of monotonic curve")
+
+    def test_energy(self):
+        """Test to check if energy of signal is being computed properly."""
+        T = np.linspace(0, 2, 200)
+        S = np.sin(2 * 2 * np.pi * T)
+        res = energy(S)
+        self.assertEqual(type(res), np.float64, "Default data type is float")
+
+    def test_significance_apriori(self):
+        """a priori significance test."""
+        T = np.linspace(0, 2, 200)
+        S = np.sin(2 * 2 * np.pi * T)
+        energy_density = energy(S) / len(S)
+        res = significance_apriori(energy_density, 2, len(S), 0.9)
+        self.assertEqual(type(res), bool, "Default data type is bool")
+
+    def test_significance_aposteriori(self):
+        """a posteriori significance test."""
+        T = np.linspace(0, 2, 200)
+        S = np.sin(2 * 2 * np.pi * T)
+        energy_density = energy(S) / len(S)
+        res = significance_aposteriori(energy_density, 2, len(S), 0.9)
+        self.assertEqual(type(res), bool, "Default data type is bool")
+
+    def test_whitenoise_check_apriori(self):
+        """a priori whitenoise_check."""
+        T = [np.linspace(0, i, 200) for i in range(5, 0, -1)]
+        S = np.array([list(np.sin(2 * 2 * np.pi * i)) for i in T])
+        res = whitenoise_check(S, test="apriori")
+        self.assertEqual(type(res), dict or None, "Default data type is dict")
+
+    def test_whitenoise_check_apriori_alpha(self):
+        """a priori whitenoise_check with custom alpha."""
+        T = [np.linspace(0, i, 200) for i in range(5, 0, -1)]
+        S = np.array([list(np.sin(2 * 2 * np.pi * i)) for i in T])
+        res = whitenoise_check(S, test="apriori", alpha=0.99)
+        self.assertEqual(type(res), dict or None, "Default data type is dict")
+
+    def test_whitenoise_check_alpha(self):
+        """a posteriori whitenoise check with custom alpha value."""
+        T = [np.linspace(0, i, 200) for i in range(5, 0, -1)]
+        S = np.array([list(np.sin(2 * 2 * np.pi * i)) for i in T])
+        res = whitenoise_check(S, alpha=0.9)
+        self.assertEqual(type(res), dict or None, "Default data type is dict")
+
+    def test_whitenoise_check_rescaling_imf(self):
+        """a posteriori whitenoise check with custom rescaling imf."""
+        T = [np.linspace(0, i, 200) for i in range(5, 0, -1)]
+        S = np.array([list(np.sin(2 * 2 * np.pi * i)) for i in T])
+        res = whitenoise_check(S, rescaling_imf=2)
+        self.assertEqual(type(res), dict or None, "Default data type is dict")
+
+    def test_whitenoise_check_nan_values(self):
+        """whitenoise check with nan in IMF."""
+        S = np.array([np.full(100, np.NaN) for i in range(5, 0, -1)])
+        res = whitenoise_check(S)
+        self.assertEqual(res, None, "Input NaN returns None")
+
+    def test_invalid_alpha(self):
+        """Test if invalid alpha return AssertionError."""
+        S = np.array([np.full(100, np.NaN) for i in range(5, 0, -1)])
+        self.assertRaises(AssertionError, whitenoise_check, S, alpha=1)
+        self.assertRaises(AssertionError, whitenoise_check, S, alpha=0)
+        self.assertRaises(AssertionError, whitenoise_check, S, alpha=-10)
+        self.assertRaises(AssertionError, whitenoise_check, S, alpha=2)
+        self.assertRaises(AssertionError, whitenoise_check, S, alpha="0.5")
+
+    def test_invalid_test(self):
+        """Test if invalid test return AssertionError."""
+        S = np.array([np.full(100, np.NaN) for i in range(5, 0, -1)])
+        self.assertRaises(AssertionError, whitenoise_check, S, test="apri")
+        self.assertRaises(AssertionError, whitenoise_check, S, test="apost")
+
+    def test_invalid_input_type(self):
+        """Test if invalid input type return AssertionError."""
+        S = [np.full(100, np.NaN) for i in range(5, 0, -1)]
+        self.assertRaises(AssertionError, whitenoise_check, S)
+        self.assertRaises(AssertionError, whitenoise_check, 1)
+        self.assertRaises(AssertionError, whitenoise_check, 1.2)
+        self.assertRaises(AssertionError, whitenoise_check, "[1,2,3,4,5]")
+
+    def test_invalid_rescaling_imf(self):
+        """Test if invalid rescaling imf return AssertionError."""
+        T = [np.linspace(0, i, 200) for i in range(5, 0, -1)]
+        S = np.array([list(np.sin(2 * 2 * np.pi * i)) for i in T])
+        self.assertRaises(AssertionError, whitenoise_check, S, rescaling_imf=10)
+        self.assertRaises(AssertionError, whitenoise_check, S, rescaling_imf=1.2)
+
+    def test_empty_input_imf(self):
+        """Test if empty IMF input return AssertionError."""
+        T1 = np.array([[], []])
+        T2 = np.array([])
+        res1 = whitenoise_check(T1)
+        res2 = whitenoise_check(T2)
+        self.assertEqual(res1, None, "Empty input returns None")
+        self.assertEqual(res2, None, "Empty input returns None")
+
+
+if __name__ == "__main__":
+    unittest.main()
