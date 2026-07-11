36eb456cf0024f634ee2c868f5fbdb5e7c9a3b3c
Dawid Laszuk <laszukdawid@gmail.com>
2026-07-11 07:06:51 -0700

    devops: automate version bumps

--- PyEMD/__init__.py
+++ PyEMD/__init__.py
@@ -1,6 +1,11 @@
 import logging
+from importlib.metadata import PackageNotFoundError, version
 
-__version__ = "1.9.0"
+try:
+    __version__ = version("EMD-signal")
+except PackageNotFoundError:
+    __version__ = "0+unknown"
+
 logger = logging.getLogger("pyemd")
 
 from PyEMD.CEEMDAN import CEEMDAN  # noqa
